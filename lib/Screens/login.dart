e6eb6b3a73ba0df85f16f3b415c219cf88a81d6b
Hammad Khan <hammad.khan0312@gmail.com>
2022-04-16 01:46:20 +0500

    second screen progresss

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
@@ -65,32 +68,167 @@ class Login extends StatelessWidget {
                             height: 16,
                           ),
                           Container(
-                            padding: EdgeInsets.only(top: 16),
-                            color: Colors.black,
-                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Colors.white),
                             child: TextField(
                               decoration: InputDecoration(
                                 contentPadding:
-                                    EdgeInsets.only(top: 19, right: 21),
+                                    EdgeInsets.only(top: 19, bottom: 18),
                                 isDense: true,
-                                fillColor: Colors.white,
-                                filled: true,
-                                border: OutlineInputBorder(),
+                                // fillColor: Colors.white,
+                                // filled: true,
+                                border: InputBorder.none,
                                 hintText: 'Email Address',
                                 hintStyle: const TextStyle(
                                     fontSize: 15,
                                     fontWeight: FontWeight.w500,
                                     fontFamily: 'Poppins-Medium',
                                     color: Color(0xFF868889)),
-                                prefixIcon: Image.asset(
-                                  'assets/images/email.png',
-                                  width: 20,
-                                  height: 20,
+                                prefixIcon: Padding(
+                                  padding: const EdgeInsets.only(left: 28),
+                                  child: Image.asset(
+                                    'assets/images/email.png',
+                                    width: 20,
+                                    height: 20,
+                                  ),
+                                ),
+                                prefix: SizedBox(width: 21),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: Colors.white),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  contentPadding:
+                                      EdgeInsets.only(top: 19, bottom: 18),
+                                  isDense: true,
+                                  // fillColor: Colors.white,
+                                  // filled: true,
+                                  border: InputBorder.none,
+                                  hintText: 'Password',
+                                  hintStyle: const TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                      fontFamily: 'Poppins-Medium',
+                                      color: Color(0xFF868889)),
+                                  prefixIcon: Padding(
+                                    padding: const EdgeInsets.only(left: 28),
+                                    child: Image.asset(
+                                      'assets/images/pass.png',
+                                      width: 20,
+                                      height: 20,
+                                    ),
+                                  ),
+                                  prefix: SizedBox(width: 21),
+                                  suffixIcon: Padding(
+                                    padding: const EdgeInsets.only(right: 28),
+                                    child: Image.asset(
+                                      'assets/images/eye.png',
+                                      width: 26.48,
+                                      height: 16.88,
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                          Text("second screeen"),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 21, left: 10, right: 8),
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 4),
+                                  child: FlutterSwitch(
+                                    padding: 0,
+                                    width: 28.57,
+                                    height: 16,
+                                    toggleSize: 13,
+                                    switchBorder: Border.all(
+                                        width: 1.5, color: Colors.white),
+                                    toggleBorder: Border.all(
+                                        width: 1.5, color: Colors.grey),
+                                    inactiveColor: Colors.amber,
+                                    value: true,
+                                    onToggle: (val) => {},
+                                  ),
+                                ),
+                                const Padding(
+                                  padding:
+                                      EdgeInsets.only(left: 9.43, right: 71),
+                                  child: Text(
+                                    "Remember me",
+                                    style: TextStyle(
+                                        color: Color(0xFF868889),
+                                        fontFamily: "Poppins-Medium",
+                                        fontSize: 15),
+                                  ),
+                                ),
+                                const Text(
+                                  "Forgot password",
+                                  style: TextStyle(
+                                      color: Color(0xFF407EC7),
+                                      fontFamily: "Poppins-Medium",
+                                      fontSize: 15),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 17),
+                            child: Container(
+                              height: 60,
+                              width: 380,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: const Color(0xff6CC51D)),
+                              child: const Padding(
+                                padding: EdgeInsets.only(top: 18),
+                                child: Text(
+                                  'Login',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w600,
+                                      fontFamily: 'Poppins-SemiBold',
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text(
+                                  'Don\'t have an account?',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w400,
+                                      fontFamily: 'Poppins-Regular',
+                                      color: Color(0xFF868889)),
+                                ),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                const Text(
+                                  'Sign up',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                      fontFamily: 'Poppins-Medium',
+                                      color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
                     ),
